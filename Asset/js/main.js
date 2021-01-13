020b7f65fbdfd452f02c44fa6e2c08205a6b2243
rizwangustama <rizonegustama@gmail.com>
2021-01-13 19:14:58 +0700

    upload

--- Asset/js/main.js
+++ Asset/js/main.js
@@ -1,10 +1,23 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
     spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      }
+    }
   });
 
 
